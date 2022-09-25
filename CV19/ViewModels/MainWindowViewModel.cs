e482b23aa125a0c373809a99d7d233dbb6a106a7
Mrrebrik <itibsoft@gmail.com>
2022-09-25 09:44:44 +0300

    - create status bar and binding text to propertu status

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -4,11 +4,25 @@ namespace CV19.ViewModels
 {
 	internal class MainWindowViewModel : ViewModel
 	{
+		#region Tittle [String]
+
 		private string _title = "CV19";
 		public string Tittle
 		{
 			get => _title;
 			set => Set(ref _title, value);
 		}
+
+		#endregion
+		#region Status [String]
+
+		private string _status = "Done";
+		public string Status
+		{
+			get => _status;
+			set => Set(ref _status, value);
+		}
+
+		#endregion
 	}
 }
