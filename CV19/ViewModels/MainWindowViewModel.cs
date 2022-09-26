3d9320fb6c70ea4de4f55a228e3f889b3bfea53b
Mrrebrik <itibsoft@gmail.com>
2022-09-26 07:54:19 +0300

    - create test datas point from test visualization

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using CV19.Infrastructure.Commands;
+using CV19.Models;
 using CV19.ViewModels.Base;
+using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Input;
 
@@ -24,6 +27,16 @@ namespace CV19.ViewModels
 		{
 			get => _status;
 			set => Set(ref _status, value);
+		}
+
+		#endregion
+		#region TestDataPoints [IEnumerable]
+
+		private IEnumerable<DataPoint> _testDataPoints;
+		public IEnumerable<DataPoint> TestDataPoints
+		{
+			get => _testDataPoints;
+			set => Set(ref _testDataPoints, value);
 		}
 
 		#endregion
@@ -52,6 +65,18 @@ namespace CV19.ViewModels
 			CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
 
 			#endregion
+
+			var dataPoints = new List<DataPoint>((int) (360 / 0.1));
+			const double toRad = Math.PI / 180;
+
+			for (var x = 0d; x <= 360; x += 0.1d)
+			{
+				var y = Math.Sin(x * toRad);
+
+				dataPoints.Add(new DataPoint { XValue = x, YValue = y });
+			}
+
+			TestDataPoints = dataPoints;
 		}
 	}
 }
