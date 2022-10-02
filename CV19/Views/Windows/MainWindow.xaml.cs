9e3a5f7fd728a531f2a890f4df06be33556eb42b
Mrrebrik <itibsoft@gmail.com>
2022-10-02 13:23:37 +0700

    - base logic serarch data from target filter text; views  events click button. and refresg views data list box

--- CV19/Views/Windows/MainWindow.xaml.cs
+++ CV19/Views/Windows/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using CV19.Models.Decanat;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace CV19
 {
@@ -7,6 +11,28 @@ namespace CV19
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void CollectionViewSource_Filter(object sender, System.Windows.Data.FilterEventArgs e)
+		{
+			if (!(e.Item is Group group)) return;
+			if (group.Name is null) return;
+
+			var filterText = GroupNameFilterText.Text;
+
+			if (filterText.Length == 0) return;
+
+			if (group.Name.Contains(filterText)) return;
+			if (group.Description.Contains(filterText)) return;
+
+			e.Accepted = false;
+		}
+
+		private void Click_SearchButton(object sender, RoutedEventArgs e)
+		{
+			var button = (Button)sender;
+			var groupCollection = (CollectionViewSource)button.FindResource("GroupsCollection");
+			groupCollection.View.Refresh();
 		}
 	}
 }
