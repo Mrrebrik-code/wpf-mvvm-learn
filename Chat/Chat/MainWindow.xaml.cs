8a39988fd1079050080662d6ded1b918becb3dda
Mrrebrik <itibsoft@gmail.com>
2022-10-06 23:57:39 +0300

    - create events buttons: minimize and window state maximazed

--- Chat/Chat/MainWindow.xaml.cs
+++ Chat/Chat/MainWindow.xaml.cs
@@ -1,23 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Chat
 {
-	/// <summary>
-	/// Логика взаимодействия для MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
 		public MainWindow()
@@ -33,6 +20,17 @@ namespace Chat
 		private void Close_Button(object sender, RoutedEventArgs e)
 		{
 			Application.Current.Shutdown();
+		}
+
+		private void Minimize_Button(object sender, RoutedEventArgs e)
+		{
+			Application.Current.MainWindow.WindowState = WindowState.Minimized;
+		}
+
+		private void WindowState_Click(object sender, RoutedEventArgs e)
+		{
+			if (Application.Current.MainWindow.WindowState != WindowState.Maximized) Application.Current.MainWindow.WindowState = WindowState.Maximized;
+			else Application.Current.MainWindow.WindowState = WindowState.Normal;
 		}
 	}
 }
